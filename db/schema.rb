49594bfc34db2adb16317e0bbc12b5a44e203c14
Jay <jjshah@ncsu.edu>
2023-09-30 15:29:33 -0400

    Removed unnecessary admin feature

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_09_30_173004) do
-  create_table "admins", force: :cascade do |t|
-    t.string "username"
-    t.string "name"
-    t.string "email"
-    t.string "password"
-    t.string "phonenumber"
-    t.string "address"
-    t.string "credit_card_number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "passengers", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -32,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_30_173004) do
     t.string "credit_card_information"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin"
   end
 
   create_table "reviews", force: :cascade do |t|
